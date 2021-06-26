76bc73fdd912787a353412be0810dc65c89ff3e6
umanggoel2001 <umang.1923it1013@kiet.edu>
2021-06-26 11:53:09 +0530

    tha10 audio

--- tha10_memory_card/app.js
+++ tha10_memory_card/app.js
@@ -3,6 +3,17 @@ const numb=document.querySelector('.num');
 
 const moves=document.querySelector('.move');
 const startMatch=document.querySelector('.but');
+const start=document.querySelector('.start');
+const click=document.querySelector('.click');
+const wrong=document.querySelector('.wrong');
+const bonus=document.querySelector('.bonus');
+
+
+
+
+
+
+
 
  var hasflip=false;
  var firstclick,secondclick;
@@ -17,8 +28,8 @@ const startMatch=document.querySelector('.but');
  //start the game
 startMatch.addEventListener('click',()=>{
     document.querySelector('.landing').style.opacity=0;
-
-  
+     start.currentTime=0;
+     start.play();
 
     setTimeout(()=>{
         document.querySelector('.landing').style.display="none";
@@ -39,6 +50,9 @@ function flip(){
         return;
     }
 
+    click.currentTime=0;
+    click.play();
+
     this.classList.add('flip');
  
     if(hasflip===false){
@@ -55,18 +69,23 @@ function flip(){
              numb.innerHTML=`SCORE<br>${score}`;
             
             moves.innerHTML=`MOVES<br> ${mo}`;
-            
+            bonus.currentTime=0;
+            bonus.play();
             firstclick.removeEventListener('click',flip);
             secondclick.removeEventListener('click',flip);
         }
         //not matching
         else{
             lock=true;
+        
+           
             mo++;
  moves.innerHTML=`MOVES<br> ${mo}`;
             
 
          setTimeout(()=>{
+            wrong.currentTime=0;
+            wrong.play();
              firstclick.classList.remove('flip');
              secondclick.classList.remove('flip');
             
