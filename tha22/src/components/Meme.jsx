366f95728d8b182206293b1df88df8d80aab467b
umanggoel2001 <umang.1923it1013@kiet.edu>
2021-07-27 03:22:02 +0530

    tha22

--- tha22/src/components/Meme.jsx
+++ tha22/src/components/Meme.jsx
@@ -1,62 +1,67 @@
+import { saveAs } from "file-saver";
+import { useState } from "react";
 
-import { useState} from 'react';
+const Meme = ({ meme, setMeme }) => {
+  const [caption, setCap] = useState({
+    template_id: meme.id,
+    username: "hitBY_fortune",
+    password: "@987654321",
+    boxes: [],
+  });
 
 
-const Meme=({meme,setMeme})=>{
-    
-    const [caption,setCap]=useState({
-        template_id: meme.id,
-        username:"hitBY_fortune",
-        password:"@987654321",
-        boxes:[],
-    
 
+  const sendurl = () => {
+    let url = `https://api.imgflip.com/caption_image?template_id=${caption.template_id}&username=${caption.username}&password=${caption.password}`;
+    caption.boxes.map((i, index) => {
+      url += `&boxes[${index}][text]=${i.text}`;
+      console.log(url);
     });
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.data !== undefined) {
+          setMeme({ ...meme, url: data.data.url });
+        } else {
+          alert("fill all the fields👻");
+        }
+      });
+  };
 
+  const down=()=>{
+    
+        saveAs(meme.url, "image.jpg"); // Put your image url here.
+   
+  }
 
-    const sendurl=()=>{
-
-    
-        let url=`https://api.imgflip.com/caption_image?template_id=${caption.template_id}&username=${caption.username}&password=${caption.password}`;
-        caption.boxes.map((i,index)=>{
-            url+=`&boxes[${index}][text]=${i.text}`;
-            console.log(url);
-        });
-        fetch(url)
-        .then((res)=> res.json())
-        .then((data)=>{
-            if(data.data!==undefined){
-                setMeme({...meme,url:data.data.url});
-            }
-            else{
-                alert("fill all the fields👻")
-            }
-           
-        })
-    }
-    
-    return(
-        <div className="mem">
-             <img src={meme.url}></img>
-             <br />
-             {
-                 [...Array(meme.box_count)].map((_,index)=>
-                     <input type="text" placeholder={`Meme caption ${index}`} onChange={(e)=>{
-                         const newCaption= caption.boxes;
-                         newCaption[index]={text: e.target.value};
-                         setCap({...caption,boxes:newCaption});
-                         console.log(caption.boxes);
-                     }}  />
-                 )
-             }
-             <div>
-                 <button onClick={sendurl}>Generate Meme</button>
-                 <button onClick={()=>{
-                     setMeme(null)
-                 }}>Chosse Template</button>
-             </div>
-        </div>
-    
-    )
-}
+  return (
+    <div className="mem">
+      <img src={meme.url}></img>
+      <br />
+      {[...Array(meme.box_count)].map((_, index) => (
+        <input
+          type="text"
+          placeholder={`Meme caption ${index}`}
+          onChange={(e) => {
+            const newCaption = caption.boxes;
+            newCaption[index] = { text: e.target.value };
+            setCap({ ...caption, boxes: newCaption });
+            console.log(caption.boxes);
+          }}
+        />
+      ))}
+      <div>
+        <button onClick={sendurl}>Generate Meme</button>
+        <button
+          onClick={() => {
+            setMeme(null);
+          }}
+        >
+          Chosse Template
+        </button>
+        <button onClick={down}>Download🚀</button>
+      </div>
+    </div>
+  );
+};
 export default Meme;
